436ab774023a8f0467371dc9fd6209dd2ca53426
pra012 <83650177+pra012@users.noreply.github.com>
2021-05-13 11:52:42 +0530

    Create Convertarraylist.java

---
+++ Convertarraylist.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<String> languages= new ArrayList<>();
+
+    
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("ArrayList: " + languages);
+
+    
+    String[] arr = new String[languages.size()];
+
+    
+    languages.toArray(arr);
+    System.out.print("Array: ");
+    for(String item:arr) {
+      System.out.print(item+", ");
+    }
+  }
+}
